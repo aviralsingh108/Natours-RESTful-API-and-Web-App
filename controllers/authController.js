ced0d86d8e38e92ee39c6b321c4f72507a6ae84f
aviralsingh108 <admiralaviral@gmail.com>
2022-01-18 22:44:45 +0530

    forgot password api added

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,3 +104,16 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  //Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no User with this Email address', 404));
+  }
+  //Generate random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({validateBeforeSave: false});
+  //send it back to the email 
+});
+exports.resetPassword = (req, res, next) => {};
